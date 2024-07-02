dd044df3e9c4ca60384c769f4226854795f685aa
Vaibhav Shokeen <shokeenvaibhav280@gmail.com>
2024-07-02 09:44:29 +0530

    landing page made responsive and some additions here and there

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import ResponsiveAppBar from "./components/newNav";
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import Landing from "./components/landing/landing";
 
 function App() {
     return <Router>
         <ResponsiveAppBar/>
         <Routes>
-            {/* <Route path="/home" element={<Home />} />
-            <Route path="/about" element={<About />} />
+            <Route path="/" element={<Landing />} />
+            {/* <Route path="/about" element={<About />} />
             <Route path="/services" element={<Services />} />
             <Route path="/clients" element={<Clients />} />
             <Route path="/contacts" element={<Contacts />} /> */}
         </Routes>
-        {/* <Route path="/home" component={Home}/> */}
     </Router>
 }
 
