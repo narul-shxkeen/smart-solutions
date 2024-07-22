6d968c69c2f961ec6a99d8244de6545eb96e5958
Vaibhav Shokeen <shokeenvaibhav280@gmail.com>
2024-07-22 23:27:28 +0530

    some adjustments to about us landing page

--- src/components/about/about_landing.jsx
+++ src/components/about/about_landing.jsx
@@ -2,12 +2,12 @@ import react from "react";
 
 
 function Landing(){
-    return <div className="flex flex-col-reverse md:flex-row gap-8 bg-[#37393A] ">
+    return <div className="flex flex-col-reverse md:flex-row gap-8 bg-[#37393A]">
         <img src="images/landingImage2.jpeg" alt="image of biggest printing machine" className="w-full md:w-1/2 h-1/2 md:h-[95vh] object-cover"/>
-        <div className="flex justify-between order-1 md:order-2 py-7 md:py-0">
+        <div className="flex justify-between order-1 md:order-2 py-7 md:py-0 h-[200vh] md:h-auto">
         <div className="absolute md:relative flex flex-col gap-6 justify-start md:justify-center w-full px-14 md:px-0 py-10 items-center md:items-start md:w-5/6 ">
         <h3 className="poppins-semibold text-base sm:text-4xl text-[#FFA630] mx-0">About Us</h3>
-        <p className="text-white poppins-regular text-sm sm:text-lg z-20">Welcome to Smart Solutions Print N Pack, a distinguished leader in the printing and advertising materials industry, proudly serving from vibrant Mayapuri Industrial Area, Phase 1, for<span className="gradient-text"> over 30 years</span>. Our mission is to provide our esteemed clients with the best-adapted and innovative substrate solutions for all their advertising and packaging needs.
+        <p className="text-white poppins-regular text-sm sm:text-lg z-20 text-justify md:text-left">Welcome to Smart Solutions Print N Pack, a distinguished leader in the printing and advertising materials industry, proudly serving from vibrant Mayapuri Industrial Area, Phase 1, for<span className="gradient-text"> over 30 years</span>. Our mission is to provide our esteemed clients with the best-adapted and innovative substrate solutions for all their advertising and packaging needs.
 
 At Smart Solutions Print N Pack, we specialize in innovative and sustainable substrate manufacturing. Our expertise encompasses plastic films that excel in printability, viewability, durability, and cost-effectiveness. Our unwavering commitment to excellence and superior quality in every project we undertake has made us a trusted name in the printing and advertising industry. 
 
